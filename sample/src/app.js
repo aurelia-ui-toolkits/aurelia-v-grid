62b8ba6f872cfbe140d9299abfa68d94393bffd9
Daniel Bendel <daniel.bendel@gmail.com>
2016-06-02 08:14:52 +0200

    chore(sample): add some routes and content

--- sample/src/app.js
+++ sample/src/app.js
@@ -1,1 +1,12 @@
-export class App {}
+export class App {
+  configureRouter(config, router) {
+    config.title = 'v-grid';
+
+    config.map([
+      { name: 'about', route: ['', 'about'], moduleId: 'about/about', title: 'About', nav: true },
+      { name: 'installation', route: 'installation', moduleId: 'installation/installation', title: 'Installation', nav: true }
+    ]);
+
+    this.router = router;
+  }
+}
