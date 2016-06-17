14ea145d6a9d3f89bc592811d24b17eb29bd0e01
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-06-17 18:21:48 +0200

    chore(sample) sample for v-grid-row-repeat

--- sample/src/samples/index.js
+++ sample/src/samples/index.js
@@ -5,7 +5,7 @@ export class MainIndex {
       { name: 'simple-html-col', route: 'simple-html-col', moduleId: './simple-html-col/index', nav: true, title: 'Simple Column Html' },
       { name: 'column-bind', route: 'column-bind', moduleId: './column-bind/index', nav: true, title: 'column.bind="myColumnConfig"' },
       { name: 'custom-html', route: 'custom-html', moduleId: './custom-html/index', nav: true, title: 'Custom Column html' },
-      // { name: 'row-repeat', route: 'row-repeat', moduleId: './row-repeat/index', nav: true, title: 'Row repeat (no column)' }, //todo
+       { name: 'row-repeat', route: 'row-repeat', moduleId: './row-repeat/index', nav: true, title: 'Row repeat (no columns)' }, //todo
        { name: 'remote', route: 'other', moduleId: './remote/index', nav: true, title: 'remote' },//todo
       { name: 'default', route: '', redirect: 'simple-html-col' }
     ]);
