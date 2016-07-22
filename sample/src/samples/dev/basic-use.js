00c9d4195ac3c7bce7328e2f03ab08f6e1df86bf
vegarringdal <vegar.ringdal@gmail.com>
2016-07-22 12:00:23 +0200

    chore(sample): update dev sample (internal use only)

--- sample/src/samples/dev/basic-use.js
+++ sample/src/samples/dev/basic-use.js
@@ -42,5 +42,120 @@ export class BasicUse {
     this.myGrid.ctx.showOnlyNotSelected();
   }
 
+  setColumns(){
+    this.myGrid.ctx.setColumns(this.columnsOption1)
+    this.myGrid.ctx.reGenerateColumns();
+  }
+
+
+
+  setColumns2(){
+    this.myGrid.ctx.setColumns(this.columnsOption2)
+    this.myGrid.ctx.reGenerateColumns();
+  }
+
+
+  singleClick(e) {
+    console.log("click")
+  }
+
+
+  singleDblClick(e) {
+    console.log("dblClick")
+  }
+
+  // sort1(){
+  //   let sortArray = [
+  //     {attribute:"name", asc:true},
+  //     {attribute:"number", asc:false}
+  //   ]
+  //
+  //   this.myGrid.ctx.orderBy(sortArray);
+  //
+  // }
+
+
+  sort1(){
+    var columns = this.myGrid.ctx.getColumns();
+    columns.pop();
+    this.myGrid.ctx.setColumns(columns)
+    this.myGrid.ctx.reGenerateColumns();
+
+
+
+  }
+
+  sort2(){
+    let sortArray = [
+      {attribute:"name", asc:false},
+      {attribute:"number", asc:false}
+    ]
+
+    this.myGrid.ctx.orderBy(sortArray);
+
+  }
+
+  columnsOption1 = [
+    {
+      colWidth:122,
+      colField:"name"
+    },
+    {
+      colWidth:122,
+      colField:"bool"
+    },
+    {
+      colWidth:122,
+      colField:"number"
+    }]
+
+
+  columnsOption2 = [
+    {
+      colWidth: 100,
+      colRowTemplate: null,
+      colHeaderTemplate: null,
+      colField: "name",
+      colHeaderName: "Full name",
+      colAddLabelAttributes: "",
+      colAddFilterAttributes: "",
+      colAddRowAttributes: "",
+      colSort: "name",
+      colFilter: true,
+      colFilterTop: false,
+      colCss: "",
+      colType: "text"
+    },
+    {
+      colWidth: 100,
+      colRowTemplate: null,
+      colHeaderTemplate: null,
+      colField: "bool",
+      colHeaderName: "Bool value",
+      colAddLabelAttributes: "",
+      colAddFilterAttributes: "",
+      colAddRowAttributes: "",
+      colSort: null,
+      colFilter: true,
+      colFilterTop: false,
+      colCss: "",
+      colType: "checkbox"
+    }, {
+      colWidth: 100,
+      colRowTemplate: null,
+      colHeaderTemplate: null,
+      colField: "number",
+      colHeaderName: "Salery",
+      colAddLabelAttributes: "",
+      colAddFilterAttributes: "",
+      colAddRowAttributes: "",
+      colSort: null,
+      colFilter: true,
+      colFilterTop: false,
+      colCss: "color:${tempRef.numberColor};font-weight:${tempRef.numberFont}",
+      colType: "text"
+    }]
+
+
 
 }
